ac872a0f175645836ba5679ed8bf889efd594b6c
David Gailey <dave.g.github@gmail.com>
2013-07-16 13:07:10 -0400

    update package and webjs to use fs

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var indexContents = fs.readFile('index.html', function (err, data) {
+		if (err) throw err;
+		console.log(data);
+		response.send('data: ' + data);
+	});
+	
 });
 
 var port = process.env.PORT || 5000;
+//var url = 'http://localhost:' + port + '/';
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
