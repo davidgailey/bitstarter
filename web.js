9e1de4ec56d9c759c1d5964fa89a85fe865845aa
David Gailey <dave.g.github@gmail.com>
2013-07-16 13:11:33 -0400

    cleanup

--- web.js
+++ web.js
@@ -7,13 +7,12 @@ app.get('/', function(request, response) {
 	var indexContents = fs.readFile('index.html', function (err, data) {
 		if (err) throw err;
 		console.log(data);
-		response.send('data: ' + data);
+		response.send(data);
 	});
 	
 });
 
 var port = process.env.PORT || 5000;
-//var url = 'http://localhost:' + port + '/';
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
